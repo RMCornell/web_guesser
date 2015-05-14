3db63dd83dcd01dade180673c6fb4bd1eba7ad7c
kurohyou108 <robert.cornell78@gmail.com>
2015-05-14 10:54:01 -0400

    Guess Checker Functioning

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,10 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-random = rand(100)
+rand = rand(10)
 
 get '/' do
-  @random_number = random
+  @random_number = rand
+  @guess = params["guess"].to_i
+  @message = guess_checker
   erb :index
 end
+
+def guess_checker
+  if @guess > @random_number
+    "Your guess of #{@guess} was too high!"
+  elsif @guess < @random_number
+    "Your guess of #{@guess} was too low!"
+  else @guess == @random_number
+    "Well Played!"
+  end
+end
+
 
